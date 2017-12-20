f22165dd3b0f543e8342e1af2bf55a7dd5ff4e9b
Antoine Gallix <antoine.gallix@gmail.com>
2017-12-20 18:11:40 +0100

    test for note put and get

--- test_app.py
+++ test_app.py
@@ -1,2 +1,20 @@
-def test_dummy():
-    pass
+from pytest import fixture
+from flask import current_app
+import json
+import tireta
+
+
+@fixture
+def app():
+    return tireta.app
+
+
+def test_client_exist(client):
+    assert client is not None
+
+
+def test_note_put(client):
+    note = {'note': 'fligeling'}
+    client.put('/note_1000', data=note, content_type='application/json')
+    response = client.get('/note_1000')
+    assert response.status_code == 200
