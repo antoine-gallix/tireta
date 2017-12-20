3eb34ac5adf3a9d726593205851fa00611f0f65f
Antoine Gallix <antoine.gallix@gmail.com>
2017-12-20 03:52:23 +0100

    first note endpoint

--- tireta.py
+++ tireta.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
+
+notes = {}
 
 
-@app.route('/')
-def mille():
-    return 'mille'
+class Note(Resource):
 
+    def get(self, note_id):
+        return {note_id: notes[note_id]}
+
+    def put(self, note_id):
+        app.logger.debug(request.data)
+        data = request.get_json()
+        notes[note_id] = data['note']
+        return {note_id: notes[note_id]}
+
+
+api.add_resource(Note, '/<string:note_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
