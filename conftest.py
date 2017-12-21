fe755c12ad338c4cc1e513a214eb5f4e09d2c9bf
Antoine Gallix <antoine.gallix@gmail.com>
2017-12-21 04:36:43 +0100

    REFACTOR : move json client test definition to fixture

--- conftest.py
+++ conftest.py
@@ -1,10 +1,38 @@
+from flask.testing import FlaskClient
 from pytest import fixture
 from tireta import create_app, db
+import json
+
+
+class JSON_Client(FlaskClient):
+    """Test client that sends json payloads
+
+    instead of:
+
+        test_client.post(some_url,
+                        content_type='application/json',
+                        data=json.dumps(some_dict))
+
+    just do:
+
+        json_test_client.post(some_url,
+                        data=some_dict)
+
+    """
+
+    def open(self, *args, **kwargs):
+        if 'data' in kwargs:
+            kwargs['data'] = json.dumps(kwargs['data'])
+            return super().open(*args, **kwargs,
+                                content_type='application/json')
+        else:
+            return super().open(*args, **kwargs)
 
 
 @fixture
 def app():
     app = create_app()
+    app.test_client_class = JSON_Client
     context = app.app_context()
     context.push()
     db.drop_all()
--- tireta.py
+++ tireta.py
@@ -3,37 +3,11 @@ import flask_sqlalchemy
 import flask_restless
 import json
 from pathlib import Path
-from flask.testing import FlaskClient
 
 here = Path(__file__).resolve().parent
 
 db = flask_sqlalchemy.SQLAlchemy()
 api = flask_restless.APIManager()
-
-
-class JSON_Client(FlaskClient):
-    """Test client that sends json payloads
-
-    instead of:
-
-        test_client.post(some_url,
-                        content_type='application/json',
-                        data=json.dumps(some_dict))
-
-    just do:
-
-        json_test_client.post(some_url,
-                        data=some_dict)
-
-    """
-
-    def open(self, *args, **kwargs):
-        if 'data' in kwargs:
-            kwargs['data'] = json.dumps(kwargs['data'])
-            return super().open(*args, **kwargs,
-                                content_type='application/json')
-        else:
-            return super().open(*args, **kwargs)
 
 
 class Note(db.Model):
@@ -46,7 +20,6 @@ def create_app():
     app = flask.Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{here}/tireta.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.test_client_class = JSON_Client
 
     db.init_app(app)
     api.init_app(app=app, flask_sqlalchemy_db=db)
