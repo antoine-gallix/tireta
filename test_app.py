9bff867fc8319337ccead33a28bb9df88fca0355
Antoine Gallix <antoine.gallix@gmail.com>
2017-12-21 02:53:59 +0100

    introduce modified test client for json api testing

--- test_app.py
+++ test_app.py
@@ -14,8 +14,8 @@ def app():
 
 
 def make_note_payload():
-    return json.dumps({'name': ''.join(random.choices(string.ascii_lowercase, k=5)),
-                       'body': ''.join(random.choices(string.ascii_lowercase, k=50))})
+    return {'name': ''.join(random.choices(string.ascii_lowercase, k=5)),
+            'body': ''.join(random.choices(string.ascii_lowercase, k=50))}
 
 # -----------------------------------------------
 
@@ -35,15 +35,12 @@ def test_get_non_existing_note(client):
 
 
 def test_note_post_note(client):
-    response = client.post('/api/note', data=make_note_payload(),
-                           content_type='application/json')
+    response = client.post('/api/note', data=make_note_payload())
     assert response.status_code == 201
 
 
-@mark.dev
 def test_note_and_get(client):
-    response = client.post('/api/note', data=make_note_payload(),
-                           content_type='application/json')
+    response = client.post('/api/note', data=make_note_payload())
     assert response.status_code == 201
     note_id = response.json['id']
     response = client.get(f'/api/note/{note_id}')
--- tireta.py
+++ tireta.py
@@ -1,14 +1,43 @@
 import flask
 import flask_sqlalchemy
 import flask_restless
+import json
 from pathlib import Path
 app = flask.Flask(__name__)
+from flask.testing import FlaskClient
 
 here = Path(__file__).resolve().parent
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{here}/tireta.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = flask_sqlalchemy.SQLAlchemy(app)
 api = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
+
+
+class JSON_Client(FlaskClient):
+    """Test client that sends json payloads
+
+    instead of:
+
+        test_client.post(some_url,
+                        content_type='application/json',
+                        data=json.dumps(some_dict))
+
+    just do:
+
+        json_test_client.post(some_url,
+                        data=some_dict)
+
+    """
+
+    def open(self, *args, **kwargs):
+        if 'data' in kwargs:
+            kwargs['data'] = json.dumps(kwargs['data'])
+            return super().open(*args, **kwargs,
+                                content_type='application/json')
+        else:
+            return super().open(*args, **kwargs)
+
+app.test_client_class = JSON_Client
 
 
 class Note(db.Model):
