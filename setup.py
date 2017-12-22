998dfe8fa279a92f6ea77dc037b83dbfe446b628
Antoine Gallix <antoine.gallix@gmail.com>
2017-12-22 14:15:59 +0100

    PACKAGE : add package name to setup

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from setuptools import setup
 
-setup(name='tireta')
+setup(
+    name='tireta',  # name under which the package is installed
+    packages=['tireta']  # which packages of the project dir to install
+)
