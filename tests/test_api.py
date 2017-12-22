a032912e0a80e006b34df596319f9ff465e521a6
Antoine Gallix <antoine.gallix@gmail.com>
2017-12-22 18:53:17 +0100

    REFACTOR : simplify tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -37,16 +37,65 @@ def user_id(client):
     response = client.post('/api/user', data=payload)
     return response.json['id']
 
+
+@fixture
+def note_id(client, user_id):
+    """ID of a registered note"""
+    payload = build_note(user_id=user_id)
+    response = client.post('/api/note', data=payload)
+    return response.json['id']
+
 # ---------------------CLIENT--------------------------
 
 
 def test_client_exist(client):
     assert client is not None
 
+# ---------------------USER--------------------------
+
+
+def test_user_resource_exist(client):
+    response = client.get('/api/user')
+    assert response.status_code == 200
+
+
+def test_test_start_with_no_user(client):
+    response = client.get('/api/user')
+    assert response.json["num_results"] == 0
+
+
+def test_get_non_existing_user(client):
+    response = client.get('/api/user/1000')
+    assert response.status_code == 404
+
+
+def test_user_post_user(client):
+    payload = build_user()
+    response = client.post('/api/user', data=payload)
+    assert response.status_code == 201
+
+
+def test_get_user(client, user_id):
+    response = client.get(f'/api/user/{user_id}')
+    assert response.status_code == 200
+
+
+def test_delete_user(client, user_id):
+    response = client.delete(f'/api/user/{user_id}')
+    assert response.status_code == 204
+    response = client.get(f'/api/user/{user_id}')
+    assert response.status_code == 404
+
+
 # ---------------------NOTES--------------------------
 
 
-def test_no_notes_at_startup(client):
+def test_note_resource_exist(client):
+    response = client.get('/api/note')
+    assert response.status_code == 200
+
+
+def test_test_start_with_no_note(client):
     response = client.get('/api/note')
     assert response.json["num_results"] == 0
 
@@ -62,40 +111,13 @@ def test_note_post_note(client, user_id):
     assert response.status_code == 201
 
 
-def test_post_and_get_note(client, user_id):
-    payload = build_note(user_id=user_id)
-    response = client.post('/api/note', data=payload)
-    assert response.status_code == 201
-    note_id = response.json['id']
+def test_get_note(client, note_id):
     response = client.get(f'/api/note/{note_id}')
     assert response.status_code == 200
 
-# ---------------------USER--------------------------
 
-
-def test_user_resource_exist(client):
-    response = client.get('/api/user')
-    assert response.status_code == 200
-
-
-def test_no_users_at_startup(client):
-    response = client.get('/api/user')
-    assert response.json["num_results"] == 0
-
-
-def test_get_non_existing_user(client):
-    response = client.get('/api/user/1000')
+def test_delete_note(client, note_id):
+    response = client.delete(f'/api/note/{note_id}')
+    assert response.status_code == 204
+    response = client.get(f'/api/note/{note_id}')
     assert response.status_code == 404
-
-
-def test_user_post_user(client):
-    response = client.post('/api/user', data=build_user())
-    assert response.status_code == 201
-
-
-def test_post_and_get_user(client):
-    response = client.post('/api/user', data=build_user())
-    assert response.status_code == 201
-    user_id = response.json['id']
-    response = client.get(f'/api/user/{user_id}')
-    assert response.status_code == 200
