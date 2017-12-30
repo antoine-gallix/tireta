ec7bcf3ecace3997fef111bd5a6faa087b87eea4
Antoine Gallix <antoine.gallix@gmail.com>
2017-12-30 04:52:57 +0100

    UTIL : update dev script

--- dev.py
+++ dev.py
@@ -1,6 +1,11 @@
 from tireta.models import User, Note, Tag
 from tireta import db, create_app
+from tests.db_utils import *
+from pdb import set_trace as bp
+from flask import current_app
+
+
 app = create_app()
 app.app_context().push()
-
+print(current_app.url_map)
 # -----------------------------------------------
