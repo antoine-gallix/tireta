deba8f5ab5ed04fde1b6cc28d94357f9f0196bc8
Antoine Gallix <antoine.gallix@gmail.com>
2018-01-04 18:54:09 +0100

    PACK : update dev test file

--- dev.py
+++ dev.py
@@ -1,11 +1,10 @@
 from tireta.models import User, Note, Tag
-from tireta import db, create_app
+from tireta import app, api, db
 from tests.db_utils import *
-from pdb import set_trace as bp
-from flask import current_app
 
 
-app = create_app()
 app.app_context().push()
 print(current_app.url_map)
+s = db.session()
+
 # -----------------------------------------------
