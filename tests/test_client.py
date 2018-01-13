85e05fe6f5c9789351c1bfde6cf3c9bf972cfb33
Antoine Gallix <antoine.gallix@gmail.com>
2018-01-13 19:16:52 +0100

    REF : pack client in its own module

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,7 +2,8 @@ from pdb import set_trace as bp
 from pytest import fixture
 from pathlib import Path
 from textwrap import dedent
-from tireta import client
+from tireta.client import text_lib
+
 
 TAG_MARK = '@:'
 TAGS = ['pif', 'paf', 'pouf', 'ploc']
@@ -30,12 +31,12 @@ def test_read_file(tmpfile):
     # write test file
     tmpfile.write_text(SIMPLE_TEXT)
     # read and check
-    read_text = client.get_note_text(tmpfile)
+    read_text = text_lib.get_note_text(tmpfile)
     assert read_text == SIMPLE_TEXT
 
 
 def test_extract_tags():
-    filtered_text, tags = client.extract_tags(TAGGED_TEXT, TAG_MARK)
+    filtered_text, tags = text_lib.extract_tags(TAGGED_TEXT, TAG_MARK)
     assert filtered_text == SIMPLE_TEXT
     assert len(tags) == len(TAGS)
     assert set(tags) == set(TAGS)
